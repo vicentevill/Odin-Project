d1a7574560316a415e306e4bac835a55b918628c
thekingvice <villarrealvicente92@gmail.com>
2023-11-03 19:16:25 -0700

    commit

--- odin-rps/app.js
+++ odin-rps/app.js
@@ -1,15 +1,34 @@
-let playerSection = "Rock";
+const rockButton = document.querySelector(".Rock");
+const paperButton = document.querySelector(".Paper");
+const scissorsButton = document.querySelector(".Scissors");
+
+let playerSection = "";
 
 let cpuSelection = "";
 
+rockButton.addEventListener("click", () => {
+  playerSection = "Rock";
+});
+
+paperButton.addEventListener("click", () => {
+  playerSection = "Paper";
+});
+
+scissorsButton.addEventListener("click", () => {
+  playerSection = "Scissors";
+});
+
 function calculateWinner() {
+  const cpuOptions = ["Rock", "Paper", "Scissors"];
+
   const winning = {
     Rock: ["Paper"],
     Paper: ["Scissors"],
     Scissors: ["Rock"],
   };
 
+  cpuSelection = cpuOptions[Math.floor(Math.random() * 3)];
+
+  console.log(cpuSelection);
   console.log(winning[playerSection].includes(cpuSelection));
 }
-
-calculateWinner();
