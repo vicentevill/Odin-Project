ec5d9d36750bcdea1f9c5dc03da58deab329fed6
thekingvice <villarrealvicente92@gmail.com>
2023-11-05 16:47:23 -0800

    result text logic finished

--- odin-rps/app.js
+++ odin-rps/app.js
@@ -1,10 +1,24 @@
 const rockButton = document.querySelector(".Rock");
 const paperButton = document.querySelector(".Paper");
 const scissorsButton = document.querySelector(".Scissors");
+const playButton = document.querySelector(".Play");
+const result = document.querySelector(".Result");
 
 let playerSection = "";
 
 let cpuSelection = "";
+//
+// let count = 0;
+
+// const test = document.querySelector(".test");
+
+// test.innerText = count;
+
+// test.addEventListener("click", () => {
+//   count++;
+//   test.innerText = count;
+// });
+//
 
 rockButton.addEventListener("click", () => {
   playerSection = "Rock";
@@ -16,6 +30,12 @@ paperButton.addEventListener("click", () => {
 
 scissorsButton.addEventListener("click", () => {
   playerSection = "Scissors";
+});
+
+playButton.addEventListener("click", () => {
+  if (playerSection) {
+    calculateWinner();
+  }
 });
 
 function calculateWinner() {
@@ -30,5 +50,14 @@ function calculateWinner() {
   cpuSelection = cpuOptions[Math.floor(Math.random() * 3)];
 
   console.log(cpuSelection);
-  console.log(winning[playerSection].includes(cpuSelection));
+  // console.log(winning[playerSection].includes(cpuSelection));
+  if (playerSection === cpuSelection) {
+    result.innerText = `Result: You picked '${playerSection}', your opponent picked '${cpuSelection}'. It's a tie.`;
+  } else if (winning[playerSection].includes(cpuSelection)) {
+    result.innerText = `Result: You picked '${playerSection}', your opponent picked '${cpuSelection}'. You lose.`;
+  } else if (!winning[playerSection].includes(cpuSelection)) {
+    result.innerText = `Result: You picked '${playerSection}', your opponent picked '${cpuSelection}'. You win!`;
+  }
+
+  // result.innerText = `Result: You picked '${playerSection}', your opponent picked '${cpuSelection}'`;
 }
