d2ff5a0ea86eb01878e9285d6c1c8ceeb06d9191
thekingvice <villarrealvicente92@gmail.com>
2023-11-08 22:39:03 -0800

    commit

--- odin-etch-a-sketch/app.js
+++ odin-etch-a-sketch/app.js
@@ -2,14 +2,17 @@
 //color picker
 //darkener
 //switch between hover and click
+const grid = document.querySelector(".Grid");
 
 const items = 16;
 
-for (let i = 0; i < items; i++) {
-  const item = items[i];
+for (let i = 0; i < items * items; i++) {
+  //   const item = items[i];
   // Create or manipulate HTML elements for each item
   const element = document.createElement("div");
   element.className = "Test";
   // Add the element to the DOM or perform other actions
-  document.body.appendChild(element);
+  grid.appendChild(element);
+  grid.style.gridTemplateColumns = `repeat(${items},1rem)`;
+  grid.style.gridTemplateRows = `repeat(${items},1rem)`;
 }
