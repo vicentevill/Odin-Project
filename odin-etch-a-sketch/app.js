bae3d0d57e0e988d2bd67c001bb8527cb3629f76
thekingvice <villarrealvicente92@gmail.com>
2023-11-09 18:56:44 -0800

    grid range input done

--- odin-etch-a-sketch/app.js
+++ odin-etch-a-sketch/app.js
@@ -3,16 +3,31 @@
 //darkener
 //switch between hover and click
 const grid = document.querySelector(".Grid");
+const gridRange = document.querySelector("#Grid-range");
+const label = document.querySelector("label");
 
-const items = 16;
+let items = gridRange.value;
+label.innerHTML = items;
 
-for (let i = 0; i < items * items; i++) {
-  //   const item = items[i];
-  // Create or manipulate HTML elements for each item
-  const element = document.createElement("div");
-  element.className = "Test";
-  // Add the element to the DOM or perform other actions
-  grid.appendChild(element);
-  grid.style.gridTemplateColumns = `repeat(${items},1rem)`;
-  grid.style.gridTemplateRows = `repeat(${items},1rem)`;
-}
+gridRange.addEventListener("input", () => {
+  items = gridRange.value;
+  label.innerHTML = items;
+  console.log(items);
+  generateGrid();
+});
+
+generateGrid = () => {
+  for (let i = 0; i < items * items; i++) {
+    //   const item = items[i];
+    // Create or manipulate HTML elements for each item
+    const element = document.createElement("div");
+    element.className = "Test";
+    // Add the element to the DOM or perform other actions
+    grid.appendChild(element);
+    grid.style.gridTemplateColumns = `repeat(${items},1fr)`;
+    grid.style.gridTemplateRows = `repeat(${items},1fr)`;
+  }
+};
+
+//init
+generateGrid();
