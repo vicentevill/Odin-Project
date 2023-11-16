a557a14f1e832a06f2655791839eb4607ae919de
thekingvice <villarrealvicente92@gmail.com>
2023-11-15 20:43:17 -0800

    Eraser added

--- odin-etch-a-sketch/app.js
+++ odin-etch-a-sketch/app.js
@@ -2,17 +2,20 @@ const Run = document.querySelector(".Run");
 Run.addEventListener("click", () => {
   resetCellColors();
 });
-//expandable grid
-//color picker
-//darkener
-//switch between hover and click
+
 const grid = document.querySelector(".Grid");
 const gridRange = document.querySelector("#Grid-range");
 const label = document.querySelector("label");
 const brushColor = document.querySelector("#Brush-color");
 const reset = document.querySelector(".Reset");
+const eraser = document.querySelector(".Eraser");
+const darkener = document.querySelector(".Darkener");
 
 let isMouseDown = false;
+
+let isEraser = false;
+
+let isDarkener = false;
 
 let filterValues = [];
 
@@ -24,6 +27,11 @@ document.addEventListener("mousedown", () => {
 document.addEventListener("mouseup", () => {
   isMouseDown = false;
   console.log(isMouseDown);
+});
+
+eraser.addEventListener("click", () => {
+  isEraser = !isEraser;
+  isDarkener = false;
 });
 
 let items = gridRange.value;
@@ -40,48 +48,61 @@ reset.addEventListener("click", () => {
   resetCellColors();
 });
 
-generateGrid = () => {
+darkener.addEventListener("click", () => {
+  isDarkener = !isDarkener;
+  isEraser = false;
+});
+
+const generateGrid = () => {
+  //Reset filterValues
   filterValues = [];
+
   for (let i = 0; i < items * items; i++) {
-    //   const item = items[i];
-    // Create or manipulate HTML elements for each item
+    //Generate grid elements and give them a class of 'Cell', then add them to Grid element
     const element = document.createElement("div");
-    element.className = "Test";
-    // Add the element to the DOM or perform other actions
+    element.className = "Cell";
     grid.appendChild(element);
+    //Template Grid
     grid.style.gridTemplateColumns = `repeat(${items},1fr)`;
     grid.style.gridTemplateRows = `repeat(${items},1fr)`;
+    //Create new filer values for each cell
     filterValues.push(1);
   }
 
-  const gridUnits = document.querySelectorAll(".Test");
-  for (let i = 0; i < gridUnits.length; i++) {
-    gridUnits[i].addEventListener("mousemove", () => {
+  //Select cell elements
+  const gridCells = document.querySelectorAll(".Cell");
+
+  //Add events for when the mouse moves over a cell
+  for (let i = 0; i < gridCells.length; i++) {
+    gridCells[i].addEventListener("mousemove", () => {
       if (isMouseDown) {
-        gridUnits[i].style.background = `${brushColor.value}`;
+        gridCells[i].style.background = `${brushColor.value}`;
+      }
+      if (isMouseDown && isEraser) {
+        gridCells[i].style.background = "";
       }
     });
-    gridUnits[i].addEventListener("mousedown", (event) => {
-      if (event.button === 0) {
-        gridUnits[i].style.background = `${brushColor.value}`;
-      } else if (event.button === 2 && filterValues[i] > 0) {
+
+    //Add events for when cell is clicked
+    gridCells[i].addEventListener("click", () => {
+      if (isDarkener) {
         filterValues[i] -= 0.1;
         filterValues[i] = Math.round(filterValues[i] * 10) / 10;
-
-        console.log(filterValues[i]);
-
-        gridUnits[i].style.filter = `brightness(${filterValues[i]})`;
-      }
+        gridCells[i].style.filter = `brightness(${filterValues[i]})`;
+      } else if (isEraser) {
+        gridCells[i].style.background = "";
+      } else gridCells[i].style.background = `${brushColor.value}`;
     });
   }
 
+  //Reset cell background colors
   resetCellColors();
 };
 
-resetCellColors = () => {
-  const gridUnits = document.querySelectorAll(".Test");
-  for (let i = 0; i < gridUnits.length; i++) {
-    gridUnits[i].style.background = "unset";
+const resetCellColors = () => {
+  const gridCells = document.querySelectorAll(".Cell");
+  for (let i = 0; i < gridCells.length; i++) {
+    gridCells[i].style.background = "";
   }
 };
 
