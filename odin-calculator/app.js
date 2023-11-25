285724aa307716a57e26dfb5bb810e66e6afb200
thekingvice <villarrealvicente92@gmail.com>
2023-11-24 19:58:56 -0800

    backspace works

--- odin-calculator/app.js
+++ odin-calculator/app.js
@@ -1,4 +1,5 @@
 const numInput = document.querySelectorAll(".numInput");
+const backSpace = document.querySelector(".backspace");
 const Test = document.querySelector(".Test");
 
 for (let i = 0; i < numInput.length; i++) {
@@ -6,6 +7,10 @@ for (let i = 0; i < numInput.length; i++) {
     appendNumber(numInput[i].innerHTML);
   });
 }
+
+backSpace.addEventListener("click", () => {
+  backSpaceDelete();
+});
 
 Test.addEventListener("click", () => {
   appendOperator(Test.innerHTML);
@@ -15,15 +20,14 @@ let expression = "";
 
 const appendNumber = (number) => {
   expression += `${number}`;
-  console.log(expression);
-  console.log(eval(expression));
+  //   console.log(expression);
+  //   console.log(eval(expression));
 };
 
 const appendOperator = (operator) => {
   if (expression) {
     expression += ` ${operator} `;
   }
-  console.log(expression);
 };
 
 const clearExpression = () => {
@@ -32,7 +36,20 @@ const clearExpression = () => {
 
 appendNumber(1);
 appendOperator("+");
-appendNumber(1);
-// clearExpression();
+// appendNumber(1);
 
-console.log(eval(expression));
+// console.log(eval(expression));
+const backSpaceDelete = () => {
+  console.log(expression);
+  console.log(expression.length);
+  console.log(expression[expression.length - 1]);
+  console.log(expression[expression.length - 1] === " ");
+  if (expression[expression.length - 1] === " ") {
+    console.log("success");
+    expression = expression.slice(0, -3);
+    console.log(expression);
+    console.log(expression.length);
+  } else {
+    expression = expression.slice(0, -3);
+  }
+};
