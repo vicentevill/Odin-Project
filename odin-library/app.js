5fcfc1097ca6e15a1976f980fae20fa693ecbb1c
thekingvice <villarrealvicente92@gmail.com>
2023-12-17 16:09:42 -0800

    commit

--- odin-library/app.js
+++ odin-library/app.js
@@ -1,30 +1,39 @@
+//Initial Rendering
 let library = [];
 
-function Book(title, author, currentPage) {
+function Book(title, author, currentPage, id) {
   this.title = title;
   this.author = author;
   this.currentPage = currentPage;
+  this.id = id;
 }
-
-let newBook = new Book("Harry Potter", "JK Rowling", "100");
-
-library.push(newBook);
-
-console.log(library);
 
 //Library List rendering
 const libraryList = document.querySelector(".libraryList");
 
 const populateLibraryList = () => {
+  libraryList.innerHTML = "";
   for (let i = 0; i < library.length; i++) {
     const element = document.createElement("li");
     element.className = "book";
-    element.innerHTML = `<h1>${library[i].title}</h1><h1>${library[i].author}</h1><h1>${library[i].currentPage}</h1>`;
+    element.innerHTML = `<h1>${library[i].title}</h1><h1>${library[i].author}</h1><h1>${library[i].currentPage}</h1><h1>id ${library[i].id}</h1><div class="delete">Delete</div>`;
     libraryList.appendChild(element);
+  }
+  const deleteButtons = document.querySelectorAll(".delete");
+
+  for (let i = 0; i < deleteButtons.length; i++) {
+    let index = i;
+    deleteButtons[i].addEventListener("click", () => {
+      library.splice(index, 1);
+      populateLibraryList();
+      console.log(library);
+    });
   }
 };
 
 populateLibraryList();
+
+//End of Initial Rendering
 
 //Adding new book
 const titleInput = document.querySelector(".titleInput");
@@ -32,11 +41,25 @@ const authorInput = document.querySelector(".authorInput");
 const pageInput = document.querySelector(".pageInput");
 const submit = document.querySelector(".submit");
 
-submit.addEventListener("click", () => {
-  library = [];
-  let book = new Book(titleInput.value, authorInput.value, pageInput.value);
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (titleInput.value && authorInput.value && pageInput.value) {
+    //id not needed
+    let newId = 0;
+    if (library.length !== 0) {
+      newId = library[library.length - 1].id + 1;
+    }
+    //id not needed
+    const book = new Book(
+      titleInput.value,
+      authorInput.value,
+      pageInput.value,
+      newId
+    );
 
-  library.push(book);
-  console.log(library);
-  populateLibraryList();
+    library.push(book);
+    populateLibraryList();
+  }
 });
+
+//Deleting Book
