bf6464b46132e4cb390650dc743a09c7497a6e8f
thekingvice <villarrealvicente92@gmail.com>
2023-12-18 15:12:53 -0800

    page edit added

--- odin-library/app.js
+++ odin-library/app.js
@@ -16,17 +16,50 @@ const populateLibraryList = () => {
   for (let i = 0; i < library.length; i++) {
     const element = document.createElement("li");
     element.className = "book";
-    element.innerHTML = `<h1>${library[i].title}</h1><h1>${library[i].author}</h1><h1>${library[i].currentPage}</h1><h1>id ${library[i].id}</h1><div class="delete">Delete</div>`;
+    element.innerHTML = `
+    <h1>${library[i].title}</h1>
+    <h1>${library[i].author}</h1>
+    <h1>Page:${library[i].currentPage}</h1>
+    <div class="add1">+1 page</div>
+    <div class="minus1">-1 page</div>
+    <div class="add10">+10 pages</div>
+    <div class="minus10">-10 pages</div>
+    <h1>id ${library[i].id}</h1>
+    <div class="delete">Delete</div>`;
     libraryList.appendChild(element);
   }
   const deleteButtons = document.querySelectorAll(".delete");
+  const add1 = document.querySelectorAll(".add1");
+  const add10 = document.querySelectorAll(".add10");
+  const minus1 = document.querySelectorAll(".minus1");
+  const minus10 = document.querySelectorAll(".minus10");
 
-  for (let i = 0; i < deleteButtons.length; i++) {
-    let index = i;
+  for (let i = 0; i < library.length; i++) {
+    // let index = i;
     deleteButtons[i].addEventListener("click", () => {
-      library.splice(index, 1);
+      library.splice(i, 1);
       populateLibraryList();
-      console.log(library);
+    });
+    add1[i].addEventListener("click", () => {
+      library[i].currentPage++;
+      populateLibraryList();
+    });
+    minus1[i].addEventListener("click", () => {
+      if (library[i].currentPage >= 1) {
+        library[i].currentPage--;
+        populateLibraryList();
+      }
+    });
+    add10[i].addEventListener("click", () => {
+      library[i].currentPage += 10;
+
+      populateLibraryList();
+    });
+    minus10[i].addEventListener("click", () => {
+      if (library[i].currentPage >= 10) {
+        library[i].currentPage -= 10;
+        populateLibraryList();
+      }
     });
   }
 };
@@ -53,7 +86,7 @@ submit.addEventListener("click", (event) => {
     const book = new Book(
       titleInput.value,
       authorInput.value,
-      pageInput.value,
+      Number(pageInput.value),
       newId
     );
 
