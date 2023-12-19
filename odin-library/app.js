e98e291b67bb03ebf64d9201cae6591bad04212f
thekingvice <villarrealvicente92@gmail.com>
2023-12-19 10:08:02 -0800

    need to add max to page inputs

--- odin-library/app.js
+++ odin-library/app.js
@@ -1,4 +1,4 @@
-//Initial Rendering
+//----------Initial Rendering----------
 let library = [];
 
 function Book(title, author, currentPage, id) {
@@ -8,23 +8,25 @@ function Book(title, author, currentPage, id) {
   this.id = id;
 }
 
-//Library List rendering
+// library.push(new Book("Harry Potter", "JK Rowling", 1, 0));
 
 const populateLibraryList = () => {
   const libraryList = document.querySelector(".libraryList");
+
+  //Reset inner html of library list
   libraryList.innerHTML = "";
+  //Add books to library list
   for (let i = 0; i < library.length; i++) {
     const element = document.createElement("li");
     element.className = "book";
     element.innerHTML = `
     <h1>${library[i].title}</h1>
-    <h1>${library[i].author}</h1>
-    <h1>Current Page:${library[i].currentPage}</h1>
-    <form>
+    <p>${library[i].author}</p>
+    <p>Current Page: ${library[i].currentPage}</p>
+    <form class=pageForm>
     <input class="setPageInput" type="number" placeholder="Enter Current Page"></input>
-    <button class="setPage" type=submit">Set Current Page</button>
+    <button class="setPage" type=submit">Set Page</button>
     </form>
-    <h1>id ${library[i].id}</h1>
     <div class="delete">Delete</div>`;
     libraryList.appendChild(element);
 
@@ -32,6 +34,7 @@ const populateLibraryList = () => {
     const setPageInput = document.querySelectorAll(".setPageInput");
     const setPage = document.querySelectorAll(".setPage");
 
+    //add event listeners to delete buttons
     deleteButtons[i].addEventListener("click", () => {
       let result = confirm(
         `Are you sure you want to delete "${library[i].title}"`
@@ -41,6 +44,7 @@ const populateLibraryList = () => {
         populateLibraryList();
       }
     });
+    //add event listeners to set page buttons
     setPage[i].addEventListener("click", (e) => {
       e.preventDefault();
       if (setPageInput[i].value) {
@@ -53,33 +57,40 @@ const populateLibraryList = () => {
 
 populateLibraryList();
 
-//End of Initial Rendering
-
-//Adding new book
+//----------Adding new book----------
 const titleInput = document.querySelector(".titleInput");
 const authorInput = document.querySelector(".authorInput");
 const pageInput = document.querySelector(".pageInput");
 const submit = document.querySelector(".submit");
 
 submit.addEventListener("click", (e) => {
+  //prevent default submit behavior
   e.preventDefault();
+  //check if the add book form is complete
   if (titleInput.value && authorInput.value && pageInput.value) {
-    //create id and
+    //create unique id
     let newId = 0;
     if (library.length !== 0) {
       newId = library[library.length - 1].id + 1;
     }
-
+    //create new book object
     const book = new Book(
       titleInput.value,
       authorInput.value,
-      Number(pageInput.value),
+      pageInput.value,
       newId
     );
-
+    //add book to library
     library.push(book);
+    //reset inputs
+    titleInput.value = "";
+    authorInput.value = "";
+    pageInput.value = 0;
+    //repopulate library list
     populateLibraryList();
   }
+  //if the add book form is not complete alert client
+  else {
+    alert("Please Complete Form");
+  }
 });
-
-//Deleting Book
