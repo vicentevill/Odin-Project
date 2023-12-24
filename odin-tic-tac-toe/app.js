ebaef5435539d4d608c287fec5bdf6fbd6e7aaac
thekingvice <villarrealvicente92@gmail.com>
2023-12-24 11:50:15 -0800

    easy mode complete

---
+++ odin-tic-tac-toe/app.js
@@ -0,0 +1,35 @@
+const gameCells = document.querySelectorAll(".cell");
+
+let gameBoard = [];
+
+for (let i = 0; i < gameCells.length; i++) {
+  gameBoard[i] = "";
+  gameCells[i].addEventListener("click", () => {
+    if (!gameBoard[i]) {
+      gameBoard[i] = "X";
+      randomMove();
+      renderGame();
+    }
+  });
+}
+
+const renderGame = () => {
+  for (let i = 0; i < gameCells.length; i++) {
+    gameCells[i].innerHTML = gameBoard[i];
+  }
+};
+
+const randomMove = () => {
+  let openCells = [];
+
+  for (let i = 0; i < 9; i++) {
+    if (!gameBoard[i]) {
+      openCells.push(i);
+    }
+  }
+
+  let randomCell = Math.floor(Math.random() * openCells.length);
+  gameBoard[openCells[randomCell]] = "O";
+};
+
+renderGame();
