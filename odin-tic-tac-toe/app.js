080ec334723b1bf7b92247432686130376fad1d1
thekingvice <villarrealvicente92@gmail.com>
2023-12-25 22:03:16 -0800

    commit

--- odin-tic-tac-toe/app.js
+++ odin-tic-tac-toe/app.js
@@ -7,7 +7,7 @@ for (let i = 0; i < gameCells.length; i++) {
   gameCells[i].addEventListener("click", () => {
     if (!gameBoard[i]) {
       gameBoard[i] = "X";
-      randomMove();
+      checkWinner("X");
       renderGame();
     }
   });
@@ -32,6 +32,31 @@ const randomMove = () => {
   gameBoard[openCells[randomCell]] = "O";
 };
 
-randomMove();
+const checkWinner = (marker) => {
+  if (gameBoard[0] && gameBoard[1] && gameBoard[2] === marker) {
+    alert("Winner");
+  } else if (gameBoard[3] && gameBoard[4] && gameBoard[5] === marker) {
+    alert("Winner");
+  } else if (gameBoard[6] && gameBoard[7] && gameBoard[8] === marker) {
+    alert("Winner");
+  }
+  //
+  else if (gameBoard[2] && gameBoard[4] && gameBoard[6] === marker) {
+    alert("Winner");
+  } else if (gameBoard[0] && gameBoard[4] && gameBoard[8] === marker) {
+    alert("Winner");
+  }
+  //
+  else if (gameBoard[0] && gameBoard[3] && gameBoard[6] === marker) {
+    alert("Winner");
+  } else if (gameBoard[1] && gameBoard[4] && gameBoard[7] === marker) {
+    alert("Winner");
+  } else if (gameBoard[2] && gameBoard[5] && gameBoard[8] === marker) {
+    alert("Winner");
+  } else {
+    randomMove();
+  }
+};
+
 renderGame();
 //make check for win function
