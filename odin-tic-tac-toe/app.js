ca6f2bbf19d2d9f3ace27187546b9eff331968cf
thekingvice <villarrealvicente92@gmail.com>
2023-12-26 14:06:30 -0800

    commit

--- odin-tic-tac-toe/app.js
+++ odin-tic-tac-toe/app.js
@@ -1,13 +1,29 @@
 const gameCells = document.querySelectorAll(".cell");
+const result = document.querySelector(".result");
 
 let gameBoard = [];
+
+let playerMarker = "X";
+
+let cpuMarker = "O";
 
 for (let i = 0; i < gameCells.length; i++) {
   gameBoard[i] = "";
   gameCells[i].addEventListener("click", () => {
-    if (!gameBoard[i]) {
-      gameBoard[i] = "X";
-      checkWinner("X");
+    if (
+      !gameBoard[i] &&
+      !checkWinner(playerMarker) &&
+      !checkWinner(cpuMarker)
+    ) {
+      gameBoard[i] = playerMarker;
+      randomMove();
+      if (checkWinner(playerMarker)) {
+        result.innerHTML = "You Win!";
+      } else if (checkWinner(cpuMarker)) {
+        result.innerHTML = "You Lose.";
+      } else {
+        checkTie();
+      }
       renderGame();
     }
   });
@@ -33,30 +49,68 @@ const randomMove = () => {
 };
 
 const checkWinner = (marker) => {
-  if (gameBoard[0] && gameBoard[1] && gameBoard[2] === marker) {
-    alert("Winner");
-  } else if (gameBoard[3] && gameBoard[4] && gameBoard[5] === marker) {
-    alert("Winner");
-  } else if (gameBoard[6] && gameBoard[7] && gameBoard[8] === marker) {
-    alert("Winner");
+  if (
+    gameBoard[0] === marker &&
+    gameBoard[1] === marker &&
+    gameBoard[2] === marker
+  ) {
+    return true;
+  } else if (
+    gameBoard[3] === marker &&
+    gameBoard[4] === marker &&
+    gameBoard[5] === marker
+  ) {
+    return true;
+  } else if (
+    gameBoard[6] === marker &&
+    gameBoard[7] === marker &&
+    gameBoard[8] === marker
+  ) {
+    return true;
   }
   //
-  else if (gameBoard[2] && gameBoard[4] && gameBoard[6] === marker) {
-    alert("Winner");
-  } else if (gameBoard[0] && gameBoard[4] && gameBoard[8] === marker) {
-    alert("Winner");
+  else if (
+    gameBoard[2] === marker &&
+    gameBoard[4] === marker &&
+    gameBoard[6] === marker
+  ) {
+    return true;
+  } else if (
+    gameBoard[0] === marker &&
+    gameBoard[4] === marker &&
+    gameBoard[8] === marker
+  ) {
+    return true;
   }
   //
-  else if (gameBoard[0] && gameBoard[3] && gameBoard[6] === marker) {
-    alert("Winner");
-  } else if (gameBoard[1] && gameBoard[4] && gameBoard[7] === marker) {
-    alert("Winner");
-  } else if (gameBoard[2] && gameBoard[5] && gameBoard[8] === marker) {
-    alert("Winner");
+  else if (
+    gameBoard[0] === marker &&
+    gameBoard[3] === marker &&
+    gameBoard[6] === marker
+  ) {
+    return true;
+  } else if (
+    gameBoard[1] === marker &&
+    gameBoard[4] === marker &&
+    gameBoard[7] === marker
+  ) {
+    return true;
+  } else if (
+    gameBoard[2] === marker &&
+    gameBoard[5] === marker &&
+    gameBoard[8] === marker
+  ) {
+    return true;
   } else {
-    randomMove();
+    return false;
+  }
+};
+
+const checkTie = () => {
+  const allTrue = gameBoard.every((element) => !!element);
+  if (allTrue && !checkWinner(playerMarker) && !checkWinner(cpuMarker)) {
+    result.innerHTML = "It's a Tie.";
   }
 };
 
 renderGame();
-//make check for win function
